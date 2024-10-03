0e3a3e2f570b8afc1be39c295316d45181e3b790
kdennewith <kdennewith@gmail.com>
2024-10-02 22:06:38 -0700

    Added two Strings

--- Lizard.java
+++ Lizard.java
@@ -1,7 +1,8 @@
 public class Lizard{
   public static void main(String[] args){
     System.out.println("Hello Lizard");
-    System.out.println("Hello Iguana")
+    System.out.println("Hello Iguana");
+    System.out.println("Bye Iguana");
   }
 }
   
